e98f83363ddb2dcfae411ffa812146f3cf3b2c36
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-04-24 21:18:54 -0400

    step one of project

--- src/laurenAndrewsCodingProject/Week04CodingProject.java
+++ src/laurenAndrewsCodingProject/Week04CodingProject.java
@@ -5,8 +5,27 @@ public class Week04CodingProject {
 	public static void main(String[] args) {
 		// array of int called ages that contains the following values:
 		// 3, 9, 23, 64, 2, 8, 28, 93
-
+		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
 		
+		//now to do some math on the last element. I need to subtract 3 from 93
+		int firstSum = ages[0] - ages[ages.length -1];
+		//to get the last value you'll need its index. 
+		//The index is the length of the overall array -1 because the index starts at 0
+		System.out.println(firstSum);
+		//as expected i got -90. 
+		
+		int[] ages2 = {14, 3, 23, 9, 64, 8, 28, 93, 60};
+		int secondSum = ages2[0] - ages2[ages2.length -1];
+		System.out.println(secondSum);
+		//-46 is what it came up with. 
+		//now to find the average age from the first array, ages.
+		int sum = 0;
+		for (int age : ages) {
+			sum += age;
+		}
+		 int averageAges = sum / ages.length;
+		 System.out.println(averageAges);
+		 // the output was 28 which is the correct math.
 		
 	}
 
