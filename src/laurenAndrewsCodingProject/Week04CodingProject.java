c8a60bf7a0917a4988a4ed8539b1575523a8aa5d
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-04-24 20:55:01 -0400

    create repository with basic files

---
+++ src/laurenAndrewsCodingProject/Week04CodingProject.java
@@ -0,0 +1,13 @@
+package laurenAndrewsCodingProject;
+
+public class Week04CodingProject {
+
+	public static void main(String[] args) {
+		// array of int called ages that contains the following values:
+		// 3, 9, 23, 64, 2, 8, 28, 93
+
+		
+		
+	}
+
+}
