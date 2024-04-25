bb874da8ba0d6fdf0fbc2eb711fcec2d9edf3470
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-04-25 05:54:34 -0400

    Steps 8 and 9 of project

--- src/laurenAndrewsCodingProject/Week04CodingProject.java
+++ src/laurenAndrewsCodingProject/Week04CodingProject.java
@@ -42,6 +42,7 @@ public class Week04CodingProject {
 		 }
 		 int averageLetters = sumLetters / names.length;
 		 System.out.println("Average Letters = " + averageLetters);
+		 System.out.println("  ");
 		 
 		 //List the names
 		 String allNames = "";
@@ -49,20 +50,21 @@ public class Week04CodingProject {
 			 allNames += name + ", ";
 		 }
 		 System.out.println(allNames);
-		 
+		 System.out.println("  ");
 		 
 		 String question3 = "How do you access the last element of any array?";
 		 String answer3 = "By subtracting 1 from the length of the array to find the index";
 		 System.out.println(question3 + ":");
 		 System.out.println(answer3);
 		 System.out.println("ages[ages.length -1]");
-		 
+		 System.out.println("  ");
 		 
 		 String question4 = "How do you access the first element of any array?";
 		 String answer4 = "Simple. The index is 0";
 		 System.out.println(question4 + ":"); 
 		 System.out.println(answer4);
 		 System.out.println("ages[0]");
+		 System.out.println("  ");
 		 
 		 //Question 5: number of letters in each name
 		 System.out.println("Name Lengths Array:");
@@ -80,11 +82,23 @@ public class Week04CodingProject {
 			 nameSum += name;
 		 }
 		 System.out.println(nameSum);
+		 System.out.println("  ");
 		 
+		 //Now for Methods
 		 //Prompt 7
 		 System.out.println("Concatenate method");
 		 System.out.println(concatenateWord("Hello", 5));
+		 System.out.println("  ");
 		 
+		 //Prompt 8
+		 System.out.print("Print a full name: ");
+		 System.out.println(fullName("Lauren", "Andrews"));
+		 System.out.println("  ");
+		 //Prompt 9
+		 System.out.println("For this one, lets reuse the array of ages from earlier.");
+		 printArray(ages);
+		 System.out.println("The sum of all the ages is greater than 100.");
+		 System.out.println(sumIsGreater100(ages));
 	} 
 
 	public static String concatenateWord(String word, int n) {
@@ -95,6 +109,27 @@ public class Week04CodingProject {
 		return finalWord;
 	}
 	
+	public static String fullName(String firstName, String lastName) {
+		String fullName = firstName + " " + lastName;
+		
+		return fullName;
+	}
 	
+	public static boolean sumIsGreater100(int[] numbers) {
+		int sum = 0;
+		for (int number : numbers) {
+			sum += number;
+		}
+		if (sum > 100) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public static void printArray(int[] someArray) {
+		for (int n : someArray) {
+			System.out.print(n + " ");
+		}
+	}
 }
